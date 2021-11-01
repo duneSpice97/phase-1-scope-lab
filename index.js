1e647a332f1b607b0403528ead855c960e088b60
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-01 17:54:15 -0600

    commit tree c94c8aa565f2dbf5901942b8894a7c4cfda041f2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'bob'.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'janet';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'karen';
+}
